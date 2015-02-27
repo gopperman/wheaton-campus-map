0e4ab67a0a1994390fef99b76be766b92596f59c
Greg Opperman <gopperman@gmail.com>
2015-02-27 15:56:21 -0500

    Register location post type

--- web/app/themes/map/lib/custom.php
+++ web/app/themes/map/lib/custom.php
@@ -2,3 +2,30 @@
 /**
  * Custom functions
  */
+function create_locations() {
+	//Register custom Locations post type
+  $location_args = array(
+    'public'              =>  true,
+    'label'               =>  'Locations',
+    'publicly_queryable'  => true,
+    'show_ui'             => true,
+    'show_in_admin_bar'   => true,
+		'show_in_nav_menus'		=> true,
+    'show_in_menu'        => true,
+    'query_var'           => true,
+    'rewrite'             => array( 'slug' => 'location' ),
+    'capability_type'     => 'post',
+    'has_archive'         => true,
+    'hierarchical'        => true,
+    'menu_position'       => 4,
+		'taxonomies'					=> array('category', 'post_tag'),
+    'supports'            => array( 'custom-fields', 'revisions', 'title', 'editor', 'author', 'thumbnail', 'excerpt')
+  );
+	if (!post_type_exists( 'location' ) ) {
+	  register_post_type( 'location', $location_args);
+	} else {
+   echo 'the post type does exist';
+	}
+}
+
+add_action('init', 'create_locations');

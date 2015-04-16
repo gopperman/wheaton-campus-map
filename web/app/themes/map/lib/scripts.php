fa7004c2718e7ea1dabc7b3ff8833872d6a1ce55
Greg Opperman <gopperman@gmail.com>
2015-04-15 23:44:24 -0400

    Halfway to a building page

--- web/app/themes/map/lib/scripts.php
+++ web/app/themes/map/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e8016f54a57d97d5ab49ecf2adf15366');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd691f065ec6f09b712bc6385cf752eff');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline

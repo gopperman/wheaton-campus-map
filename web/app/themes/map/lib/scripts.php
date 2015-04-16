fd66f23cd27c34a607bdc9163d302827d0c0021a
Greg Opperman <gopperman@gmail.com>
2015-04-16 00:57:38 -0400

    Sidebar additions

--- web/app/themes/map/lib/scripts.php
+++ web/app/themes/map/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd691f065ec6f09b712bc6385cf752eff');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'efb37d082d42bf8bece723435fa37ce6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline

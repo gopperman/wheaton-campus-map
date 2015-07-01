5bfd33dbeac67841135b72e63730f4bae7216117
Greg Opperman <gopperman@gmail.com>
2015-07-01 17:24:32 -0400

    Header and Footer

--- web/app/themes/map/templates/footer.php
+++ web/app/themes/map/templates/footer.php
@@ -1,6 +1,11 @@
 <footer class="content-info" role="contentinfo">
   <div class="container-fluid">
 		&copy; <?= date("Y") ?> <a href="//wheatoncollege.edu">Wheaton College</a>
+		<div class="social" class="hidden-xs">
+    	<a href="https://www.facebook.com/WheatonCollege" target="_blank"><i class="fa fa-facebook"></i></a>
+      <a href="https://twitter.com/wheaton" target="_blank"><i class="fa fa-twitter"></i></a>
+      <a href="https://instagram.com/wheatoncollege" target="_blank"><i class="fa fa-instagram"></i></a>
+    </div>
   </div>
 </footer>
 
--- web/app/themes/map/templates/header-top-navbar.php
+++ web/app/themes/map/templates/header-top-navbar.php
@@ -1,12 +1,12 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
-  <div class="container">
+  <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+<!--      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-      </button>
+      </button>-->
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
 
@@ -17,5 +17,13 @@
         endif;
       ?>
     </nav>
+		<div id="ctas">
+			<div class="social" class="hidden-xs hidden-sm">
+				<a href="https://www.facebook.com/WheatonCollege" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i></a>
+				<a href="https://twitter.com/wheaton" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i></a>
+				<a href="https://instagram.com/wheatoncollege" target="_blank" rel="nofollow"><i class="fa fa-instagram"></i></a>
+			</div>
+			<a class="button" href="http://wheatoncollege.edu/admission/visit-campus/">Visit Campus</a>
+		</div>
   </div>
 </header>
